bbab3600fccde597091be5c88a08d8a13f8339e4
oklyz <alimodffs2@gmail.com>
2025-08-07 13:48:38 +0300

    Initalize structure

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # ShopEase-back-end
+# Date 8/7/2025
+# By: Ali Hasan, Qassim Aldearzi, Zeshan Ahmed, Abdulla Zaid
+#### Desciption
+
+##
+
+- 
+- 
+- 
+- 
+
+---
+
+### Breif Description
+
+#### 
+
+---
+
+### Entity Relationship Diagram
+
+
+
+---
+
+### _Technology Stack:_
+
+- **Back-end:** MongoDB, Node.js, Express, JavaScript
---
+++ server.js
@@ -0,0 +1,35 @@
+// imports
+const express = require("express")
+require("dotenv").config
+const app = express()
+const path = require("path")
+
+// Initailze app
+
+// Database Configuration
+const db = require("./config/db")
+
+// set Port Configuration
+const port = process.env.PORT ? process.env.PORT : 3001
+
+// Middlewares
+const logger = require("morgan")
+const cors = require("cors")
+
+// use Middlewares
+app.use(logger())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+
+//Require Routes
+
+
+// use Routes
+
+
+// Listener
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
