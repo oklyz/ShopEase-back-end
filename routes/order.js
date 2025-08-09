b8a60ad322d4c176254318c6c7ed4566e30eb5a8
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-09 15:19:17 +0300

    fix issue fet order by user ID

--- routes/order.js
+++ routes/order.js
@@ -3,5 +3,5 @@ const controller = require('../controllers/order')
 const middleware = require('../middlewares')
 router.post('/new', controller.create_order_post)
 router.get('/:orderid', controller.get_order_byID_post)
-router.get('/:userId', controller.get_orders_by_userId_get)
+router.get('/user/:userId', controller.get_orders_by_userId_get)
 module.exports = router
